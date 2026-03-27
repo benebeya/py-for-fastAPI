69274842250e25d03413be042b2c5d5f140ecf86
benebeya <m_mohamedleminesidibeya@estin.dz>
2026-03-27 11:22:12 +0100

    adding decorators

---
+++ decorators/ex.py
@@ -0,0 +1,14 @@
+"""A decorator wraps a function to add extra behavior"""
+
+def my_decorator(func):
+    def wrapper():
+        print("before")
+        func()
+        print("after")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("hello!")
+    
+say_hello()
